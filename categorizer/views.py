1a6b72d6a90841c51386f2ebb24536cb87451fdb
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2016-11-11 11:49:06 +0800

    Create project

---
+++ categorizer/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'home.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def categorizer(request):
+    return render(request, 'categorizer.html')
+
+def categorized(request):
+    return render(request, 'categorized.html')
+
+def complaints(request):
+    return render(request, 'complaints.html')
+    
+def adminpage(request):
+    return render(request, 'adminpage.html')
