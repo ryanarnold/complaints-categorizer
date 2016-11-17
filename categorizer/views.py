64f04b404d51b267c9be9bca30f65a7ec5e25ab7
Tristan Magturo <magturo.tristan@yahoo.com>
2016-11-17 20:41:37 +0800

    Add values to the table in performance
    Added values to table using actual_category and system_category. Also
    added comparison

--- categorizer/views.py
+++ categorizer/views.py
@@ -30,7 +30,7 @@ def data(request):
     return render(request, 'data.html')
     
 def performance(request):
-    context = {'accuracy': 0.0}
+    context = {'accuracy': 0.0, 'prediction': [], }
 
     if request.method == 'POST':
         complaints = []
@@ -40,20 +40,20 @@ def performance(request):
                 complaints.append({'id': row[0], 'body': row[1], 'category': row[3]})
 
         # Preprocessing:
-        start = time.time()
-        print('Preprocessing complaints...')
-        preprocessed_complaints = preprocess(complaints)
-        print('Finished after {0} seconds.'.format(time.time() - start))
+        #start = time.time()
+        #print('Preprocessing complaints...')
+        #preprocessed_complaints = preprocess(complaints)
+        #print('Finished after {0} seconds.'.format(time.time() - start))
 
-        start = time.time()
-        print('Extracting features...')
-        features = extract_features(preprocessed_complaints)
-        print('Finished after {0} seconds.'.format(time.time() - start))
+        #start = time.time()
+        #print('Extracting features...')
+        #features = extract_features(preprocessed_complaints)
+        #print('Finished after {0} seconds.'.format(time.time() - start))
 
-        start = time.time()
-        print('Vectorizing...')
-        vectorize(preprocessed_complaints, features)
-        print('Finished after {0} seconds.'.format(time.time() - start))
+        #start = time.time()
+        #print('Vectorizing...')
+        #vectorize(preprocessed_complaints, features)
+        #print('Finished after {0} seconds.'.format(time.time() - start))
 
         # Classification:
         start = time.time()
@@ -71,5 +71,18 @@ def performance(request):
         context['accuracy'] = '{0:.4f}'.format(clf.score(X_test, y_test) * 100)
         print('Finished after {0} seconds.'.format(time.time() - start))
 
+        predict_list = X.reshape(len(X), -1)
+        category_list = y
+        predictions_num = clf.predict(predict_list)
+        cats = {
+            '1': 'HR',
+            '4': 'ROADS',
+            '5': 'BRIDGES',
+            '6': 'FLOOD CONTROL'
+        }
+        for i in range(len(predictions_num)):
+            correct = 'Yes' if predictions_num[i] == category_list[i] else 'No'    
+            context['prediction'].append({'id': i + 1, 'system_category': cats[str(predictions_num[i])], 'actual_category': cats[str(category_list[i])], 'correct': correct})
+        
 
     return render(request, 'performance.html', context)
