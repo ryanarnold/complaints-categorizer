6bb1534427aef2c3890de988e69062eb09381830
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-03-19 14:01:00 +0800

    Remove virtualenv

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/arnold/Workspace/DjangoProjects/complaints_categorizer/.venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
